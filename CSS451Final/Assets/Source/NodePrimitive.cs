2517e321eef5613b0841c50e17309fe5ba833dbf
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-11-25 15:06:14 -0800

    Source, mats, and pinball base
    Copied over the source and materials from mp4 and setup the base of the pinball machine.

---
+++ CSS451Final/Assets/Source/NodePrimitive.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodePrimitive: MonoBehaviour {
+    public Color MyColor = new Color(0.1f, 0.1f, 0.2f, 1.0f);
+    public Vector3 Pivot;
+    public bool rotateFlag = false;
+    private bool forward = true;
+
+	// Use this for initialization
+	void Start () {
+    }
+
+    void Update()
+    {
+        if (rotateFlag)
+        {
+            Vector3 rot = transform.eulerAngles;
+            rot.z = 45.0f * Mathf.Sin(Time.timeSinceLevelLoad);
+            transform.eulerAngles = rot;
+        }
+    }
+	
+  
+	public void LoadShaderMatrix(ref Matrix4x4 nodeMatrix)
+    {
+        Matrix4x4 p = Matrix4x4.TRS(Pivot, Quaternion.identity, Vector3.one);
+        Matrix4x4 invp = Matrix4x4.TRS(-Pivot, Quaternion.identity, Vector3.one);
+        Matrix4x4 trs = Matrix4x4.TRS(transform.localPosition, transform.localRotation, transform.localScale);
+        Matrix4x4 m = nodeMatrix * p * trs * invp;
+        GetComponent<Renderer>().material.SetMatrix("MyXformMat", m);
+        GetComponent<Renderer>().material.SetColor("MyColor", MyColor);
+    }
+}
---
+++ CSS451Final/Assets/Source/SceneNode.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneNode : MonoBehaviour {
+
+    protected Matrix4x4 mCombinedParentXform;
+    
+    public Vector3 NodeOrigin = Vector3.zero;
+    public List<NodePrimitive> PrimitiveList;
+    public Transform AxisFrame = null;
+    public Camera secondaryCamera = null;
+    public float yOffset = 0;
+    // Use this for initialization
+    protected void Start () {
+        InitializeSceneNode();
+        // Debug.Log("PrimitiveList:" + PrimitiveList.Count);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void InitializeSceneNode()
+    {
+        mCombinedParentXform = Matrix4x4.identity;
+    }
+
+    // This must be called _BEFORE_ each draw!! 
+    public void CompositeXform(ref Matrix4x4 parentXform)
+    {
+        Matrix4x4 orgT = Matrix4x4.Translate(NodeOrigin);
+        Matrix4x4 trs = Matrix4x4.TRS(transform.localPosition, transform.localRotation, transform.localScale);
+        
+        mCombinedParentXform = parentXform * orgT * trs;
+
+        // propagate to all children
+        foreach (Transform child in transform)
+        {
+            SceneNode cn = child.GetComponent<SceneNode>();
+            if (cn != null)
+            {
+                cn.CompositeXform(ref mCombinedParentXform);
+            }
+        }
+        
+        // disenminate to primitives
+        foreach (NodePrimitive p in PrimitiveList)
+        {
+            p.LoadShaderMatrix(ref mCombinedParentXform);
+        }
+
+        // Compute AxisFrame 
+        if (AxisFrame != null)
+        {
+            AxisFrame.transform.localPosition = mCombinedParentXform.GetColumn(3);
+            AxisFrame.transform.localRotation = transform.rotation;
+            AxisFrame.transform.up = transform.up;
+        }
+
+
+        if (secondaryCamera != null)
+        {
+            Vector3 location = mCombinedParentXform.GetColumn(3);
+            location.y -= yOffset;
+            secondaryCamera.transform.localPosition = location;
+            secondaryCamera.transform.forward = -transform.up;
+        }
+    }
+}
