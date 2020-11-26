f860890cf845204f620c18ed897a0db3ac63d1a2
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-11-26 11:45:27 -0800

    Ball Launcher
    Added the ball launching mechanism

---
+++ CSS451Final/Assets/Source/Model/Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject handle;
+    private HandleScript handleScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        handleScript = handle.GetComponent<HandleScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hitInfo, 100))
+            {
+                if(hitInfo.collider.gameObject == handle)
+                {
+                    handleScript.Select(true);
+                }
+            }
+        } else
+        {
+            handleScript.Select(false);
+        }
+    }
+}
---
+++ CSS451Final/Assets/Source/Model/PistonScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistonScript : MonoBehaviour
+{
+    public GameObject start;
+    public GameObject end;
+    private Vector3 initScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var p1Pos = start.transform.position;
+        var p2Pos = end.transform.position;
+
+        var offset = p2Pos - p1Pos;
+        var scale = new Vector3(initScale.x, offset.magnitude/2, initScale.z);
+
+        var position = p1Pos + (offset / 2.0f);
+
+        transform.localScale = scale;
+        transform.position = position;
+    }
+}
